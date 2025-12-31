8aacdc1da326e26f10f8543c9665121b9c686c76
472022 <nextin.pvt@gmail.com.com>
2025-12-31 21:00:45 +0530

    deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-  }
-);
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT || 'sqlite',
+  storage: process.env.DB_STORAGE || './database.sqlite',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  logging: false,
+});
 
 module.exports = sequelize;
